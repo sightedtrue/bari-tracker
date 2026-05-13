424e92d5dda581230c7bc56cff3283ac56059b9a
sightedtrue <97sxmk5kfd@privaterelay.appleid.com>
2026-05-14 02:57:45 +0400

    Add files via upload

---
+++ bari-tracker/sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'bari-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  'https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
